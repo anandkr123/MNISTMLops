87d97e0f95733ed5430f1f9570841e04f98fdb11
anandkr123 <mauryaanand04@gmail.com>
2022-01-05 06:00:34 +0100

    added ml

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
 from model import Network
-
+import mlflow
 
 # Hyper-parameters
 
@@ -13,16 +13,20 @@ train_batch_size = 100
 
 
 def train(model, train_loader, device, optimizer):
-    model.train()
-    for batch, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
+    with mlflow.start_run(run_name="Basic run"):
+        model.train()
+        for batch, (data, target) in enumerate(train_loader):
+            data, target = data.to(device), target.to(device)
+            optimizer.zero_grad()
 
-        output = model(data)
-        loss = F.cross_entropy(output, target)
+            output = model(data)
+            loss = F.cross_entropy(output, target)
 
-        loss.backward()
-        optimizer.step()
+            loss.backward()
+            optimizer.step()
+        mlflow.log_param("Epochs", epochs)
+        mlflow.log_param("Training batch size", train_batch_size)
+        mlflow.log_metric("Loss", loss.item())
 
     #         if batch % 100 == 0:
     #             print(f"Train Epoch: {epoch} [{batch * len(data)}/{len(train_loader.dataset)}({(100. * batch / len(train_loader)):.0f}%)] \tLoss: {loss.item():.6f}")
@@ -49,6 +53,7 @@ model = Network().to(device)
 optimizer = optim.Adam(model.parameters(), lr=3e-3, weight_decay=0)
 
 
-for epoch in range(1, epochs + 1):
-    train(model, train_loader, device, optimizer)
+if __name__ == "__main__":
+    for epoch in range(1, epochs + 1):
+        train(model, train_loader, device, optimizer)
     # test(model, test_loader, device)
